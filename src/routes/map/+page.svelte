f38ca320524e694402a6fc6c80383e195aa642e6
robinlovelace <rob00x@gmail.com>
2023-11-24 23:20:51 +0000

    Attempt pmtiles

--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -7,8 +7,23 @@
     zoom={11}
     class="map"
     standardControls
-    style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json" />
-  
+    style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json" 
+>
+<VectorTileSource
+url={'pmtiles://https://r2-public.protomaps.com/protomaps-sample-datasets/cb_2018_us_zcta510_500k.pmtiles'}
+>
+<LineLayer
+  paint={{
+    'line-opacity': 0.6,
+    'line-color': 'rgb(53, 175, 109)',
+    'line-width': 2,
+  }}
+  sourceLayer={'zcta'}
+/>
+</VectorTileSource>
+</MapLibre>    
+
+
   <style>
     :global(.map) {
       height: 500px;
