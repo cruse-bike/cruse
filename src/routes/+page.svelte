9c59c3303f33117736b78a5ab383d62f1f4e6a12
robinlovelace <rob00x@gmail.com>
2023-11-26 19:52:35 +0000

    Update with map on landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
   import { base } from "$app/paths";
+  import { MapLibre, VectorTileSource, LineLayer } from 'svelte-maplibre';
 </script>
 
 <h1>sveltekit-gh-pages</h1>
@@ -7,4 +8,33 @@
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
+
+<MapLibre
+	center={[-8.63, 52.66]}
+	zoom={11}
+	class="map"
+	standardControls
+	style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+>
+    <VectorTileSource
+        url={'pmtiles://rnet_limerick.pmtiles'}
+    >
+        <LineLayer
+            paint={{
+                'line-opacity': 0.6,
+                'line-color': 'rgb(53, 175, 109)',
+                'line-width': 2
+            }}
+            sourceLayer="rnet_limerick"
+        ></LineLayer>
+    </VectorTileSource>
+</MapLibre>
+
+
+<style>
+	:global(.map) {
+		height: 500px;
+	}
+</style>
+
 <a href="{base}/about">About</a>
