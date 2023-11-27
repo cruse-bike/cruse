fbe339dfa1a2ed686a9e37c2894f6ce332f7fcde
robinlovelace <rob00x@gmail.com>
2023-11-27 23:46:02 +0000

    Working example with popups

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script>
   import { base } from "$app/paths";
-  import { MapLibre, VectorTileSource, LineLayer } from 'svelte-maplibre';
+  import { MapLibre, VectorTileSource, LineLayer, Popup } from 'svelte-maplibre';
   // Add geocoder:
   import GeocodingControl from "@maptiler/geocoding-control/svelte/GeocodingControl.svelte";
   import { createMapLibreGlMapController } from "@maptiler/geocoding-control/maplibregl";
   import maplibregl, { Map, NavigationControl } from 'maplibre-gl';
+	import { get } from "svelte/store";
   // Add your API key:
   const apiKey = 'EU1qfgGypy2AfZTKCG6c';
 </script>
@@ -29,11 +30,27 @@
         <LineLayer
             paint={{
                 'line-opacity': 0.6,
-                'line-color': 'rgb(53, 175, 109)',
+                // Style based on value of 'Bicycle (Go Dutch)' field
+                // With breaks at 0, 10, 100, 1000:
+                'line-color': [
+                    'step',
+                    ['get', 'Bicycle (Go Dutch)'],
+                    '#f2f12d',
+                    10,
+                    '#e6b71e',
+                    100,
+                    '#d84e11',
+                    1000,
+                    '#bf1d00'
+                ],
                 'line-width': 2
             }}
             sourceLayer="rnet_limerick"
-        ></LineLayer>
+            hoverCursor="pointer"        >
+        <Popup openOn="click" offset={[0, -10]}>
+            <div class="text-lg font-bold">"Test value"</div>
+        </Popup>
+        </LineLayer>
     </VectorTileSource>
     <GeocodingControl
         apiKey={apiKey}
