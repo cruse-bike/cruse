27ad441b9e3e0c611f8bf21d2b7bd01258bb4a4b
robinlovelace <rob00x@gmail.com>
2024-01-19 14:11:37 +0000

    Update, working counties

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,7 @@
 		FillLayer
 	} from 'svelte-maplibre';
 	import GeocodingControl from '@maptiler/geocoding-control/svelte/GeocodingControl.svelte';
-	import { createEventDispatcher } from 'svelte';
+	import { createEventDispatcher, onMount } from 'svelte';
 
 	const apiKey = 'EU1qfgGypy2AfZTKCG6c';
 	const dispatch = createEventDispatcher();
@@ -38,35 +38,25 @@
 		dispatch('layerChange', { layer: selectedLayer });
 	}
 
-  let networkType = 'balanced'; // Initialize networkType to 'balanced'
-  const networkTypes = ['fastest', 'balanced', 'quietest']; // Define the network types
+	let networkType = 'balanced'; // Initialize networkType to 'balanced'
+	const networkTypes = ['fastest', 'balanced', 'quietest']; // Define the network types
 
-  const lineOpacity = [
-    'interpolate',
-    ['linear'],
-    ['zoom'],
-    8,
-    0.0,
-    11,
-    1
-  ];
-
+	const lineOpacity = ['interpolate', ['linear'], ['zoom'], 8, 0.0, 11, 1];
 </script>
 
 <div class="selector-container">
-  <div class="layer-selector">
-    <select bind:value={selectedKey} on:change={() => toggleLayer(selectedKey)}>
-        {#each Object.keys(keyMap) as displayName (displayName)}
-            <option value={displayName}>{displayName}</option>
-        {/each}
-    </select>
-  </div>
-
+	<div class="layer-selector">
+		<select bind:value={selectedKey} on:change={() => toggleLayer(selectedKey)}>
+			{#each Object.keys(keyMap) as displayName (displayName)}
+				<option value={displayName}>{displayName}</option>
+			{/each}
+		</select>
+	</div>
 </div>
 
 <MapLibre
 	{apiKey}
-    center={[-7.96, 53.42]}
+	center={[-7.96, 53.42]}
 	zoom={6}
 	class="map"
 	controlPosition="top-right"
@@ -92,10 +82,16 @@
 	<FullscreenControl position="top-right" />
 	<ScaleControl />
 
-	<VectorTileSource
-    url={'pmtiles://rnet_multi_balanced.pmtiles'} 
-    minzoom=13
-  >
+	<GeoJSON id="counties" data="counties.geojson">
+		<FillLayer
+			paint={{
+				'fill-color': '#888888',
+				'fill-opacity': 0.9
+			}}
+		/>
+	</GeoJSON>
+
+	<VectorTileSource url={'pmtiles://rnet_multi_balanced.pmtiles'} minzoom="13">
 		<LineLayer
 			id="rnet"
 			paint={selectedLayer === 'Quietness'
@@ -113,36 +109,38 @@
 							'#000000'
 						],
 						'line-width': 2,
-            'line-opacity': lineOpacity
-				  } : selectedLayer === 'Gradient' ? {
-            'line-color': [
-            'case',
-            ['<=', ['to-number', ['get', 'Gradient']], 1],
-            '#8BC34A', // Light Green
-            ['<=', ['to-number', ['get', 'Gradient']], 3],
-            '#CDDC39', // Lime
-            ['<=', ['to-number', ['get', 'Gradient']], 20],
-            '#FFA500', // Lighter Orange
-            ['>=', ['to-number', ['get', 'Gradient']], 20],
-            '#FF9800',
-            '#000000'
-        ],
-            'line-width': 2,
-            'line-opacity': lineOpacity
-          }
-				: {
-						'line-color': [
-							'interpolate',
-							['linear'],
-							['get', selectedLayer],
-							10,
-							'#ADD8E6',
-							1000,
-							'#006400'
-						],
-						'line-width': 2,
-            'line-opacity': lineOpacity
-				  }}
+						'line-opacity': lineOpacity
+					}
+				: selectedLayer === 'Gradient'
+					? {
+							'line-color': [
+								'case',
+								['<=', ['to-number', ['get', 'Gradient']], 1],
+								'#8BC34A', // Light Green
+								['<=', ['to-number', ['get', 'Gradient']], 3],
+								'#CDDC39', // Lime
+								['<=', ['to-number', ['get', 'Gradient']], 20],
+								'#FFA500', // Lighter Orange
+								['>=', ['to-number', ['get', 'Gradient']], 20],
+								'#FF9800',
+								'#000000'
+							],
+							'line-width': 2,
+							'line-opacity': lineOpacity
+						}
+					: {
+							'line-color': [
+								'interpolate',
+								['linear'],
+								['get', selectedLayer],
+								10,
+								'#ADD8E6',
+								1000,
+								'#006400'
+							],
+							'line-width': 2,
+							'line-opacity': lineOpacity
+						}}
 			sourceLayer="rnet"
 			hoverCursor="pointer"
 		>
@@ -156,20 +154,6 @@
 			</Popup>
 		</LineLayer>
 	</VectorTileSource>
-
-	<GeoJSON
-		id="counties"
-		data="counties.geojson"
-		>
-		<FillLayer
-			id="counties"
-			paint={{
-				'fill-color': '#888888',
-				'fill-opacity': 0.9
-			}}
-		/>
-	</GeoJSON>
-
 </MapLibre>
 
 <style>
@@ -187,14 +171,14 @@
 		font-weight: 600;
 	}
 
-  /* Set the font to Google's prompt */
-  @import url('https://fonts.googleapis.com/css2?family=Prompt:wght@300&display=swap');
-  :global(body) {
-    font-family: 'Prompt', sans-serif;
-  }
+	/* Set the font to Google's prompt */
+	@import url('https://fonts.googleapis.com/css2?family=Prompt:wght@300&display=swap');
+	:global(body) {
+		font-family: 'Prompt', sans-serif;
+	}
 
-  .selector-container {
-    display: flex;
-    gap: 10px;
-  }
+	.selector-container {
+		display: flex;
+		gap: 10px;
+	}
 </style>
